c83f16a12c6270a486fb178f502ebd819dd01a41
dsbdo <primerplus233@gmail.com>
2018-08-27 00:35:37 +0800

    finish cluster server

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 #include<ctime>
+#include<jsoncpp/json/json.h>
+#include<string>
 int main(int argc, char** argv) {
     using namespace std;
     time_t timestamp;
     cout << time(NULL) << endl;
     cout << time(&timestamp) << endl;
     cout << timestamp << endl;
+        Json::Value root;
+    root["req_type"] = "clusterserver_join" ;
+    root["ip"] = "127.0.0.1";
+    root["port"] = "8080";
+    Json::StyledWriter writer;
+    std::string str = writer.write(root);
+    cout << str;
+    return 0;
+
 }
